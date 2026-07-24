410ed28684b53439920ea2d69df6f0ce2132f404
vikalp <kumarvikalp48@gmail.com>
2026-07-24 15:20:55 +0530

    timezone to asia/kolkata

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,9 +87,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_TZ = True
+TIME_ZONE = 'Asia/Kolkata'
+USE_TZ = True            # keep as-is
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -158,7 +158,7 @@ CELERY_RESULT_BACKEND = config('REDIS_URL', default='redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = 'Asia/Kolkata'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 EXOTEL_SID = config('EXOTEL_SID', default='')
